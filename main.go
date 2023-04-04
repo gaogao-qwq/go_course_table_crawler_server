0b959e8bdd875cb39a68858a4fcf9d06eb1e4ae4
gaogao-qwq <gaogaoqwq@gmail.com>
2023-04-04 15:09:30 +0800

    Chore: Add server starting message

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"course_table_server/internal/app/server/handlers"
+	"fmt"
 	"log"
 	"net/http"
 )
@@ -12,5 +13,6 @@ func main() {
 	mux.HandleFunc("/login", handlers.LoginHandler)
 	mux.HandleFunc("/semester-list", handlers.SemesterListHandler)
 	mux.HandleFunc("/course-table", handlers.CourseTableHandler)
+	fmt.Println("Listen and serve on port :56789")
 	log.Fatal(http.ListenAndServe(":56789", mux))
 }
